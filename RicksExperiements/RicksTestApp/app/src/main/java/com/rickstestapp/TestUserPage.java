9bf445607e17813fefd7a4f31b391c885505b6e5
Rick Twohy <twohyjr@iastate.edu>
2016-02-15 01:38:23 -0600

    Did some more experimenting

--- RicksExperiements/RicksTestApp/app/src/main/java/com/rickstestapp/TestUserPage.java
+++ RicksExperiements/RicksTestApp/app/src/main/java/com/rickstestapp/TestUserPage.java
@@ -1,13 +1,54 @@
 package com.rickstestapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.renderscript.ScriptIntrinsicLUT;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class TestUserPage extends AppCompatActivity {
+
+
+    private TextView thumbsDownText;
+    private TextView thumbsUpText;
+    private TextView ageText;
+    private TextView zipText;
+    private TextView genderText;
+    private TextView lastActiveText;
+    private TextView username;
+    private TextView friendZone;
+    private Button addFriend;
+    private ScrollView achievmentsView;
+
+
+    public Boolean secure = false;
+    public Boolean friend = false;
+    public String name = "Madman123";
+    public String age = "23";
+    public String zipcode = "12564";
+    public String gender = "Female";
+    public String lastActive = "01/20/2015";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +57,111 @@ public class TestUserPage extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        thumbsDownText = (TextView) findViewById(R.id.thumbsDownText);
+        thumbsUpText = (TextView) findViewById(R.id.thumbsUpText);
+        username = (TextView) findViewById(R.id.userNameText);
+        ageText = (TextView) findViewById(R.id.ageText);
+        zipText = (TextView) findViewById(R.id.zipText);
+        genderText = (TextView) findViewById(R.id.genderText);
+        lastActiveText = (TextView) findViewById(R.id.lastActiveDate);
+        friendZone = (TextView) findViewById(R.id.friendZoneText);
+        addFriend = (Button) findViewById(R.id.addFriendButton);
+        achievmentsView = (ScrollView) findViewById(R.id.achievmentsView);
+
+
+        username.setText(name);
+        ageText.setText(age);
+        zipText.setText(zipcode);
+        genderText.setText(gender);
+        lastActiveText.setText(lastActive);
+        getFriendZone();
+        getAchievments();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    private void getAchievments(){
+        int achievementWidth = 100;
+
+        ImageView iv = new ImageView(achievmentsView.getContext());
+        iv.setBackgroundColor(Color.RED);
+        iv.setVisibility(View.VISIBLE);
+        iv.setMinimumWidth(achievementWidth);
+        iv.setMinimumHeight(achievementWidth);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(achievementWidth,achievementWidth);
+        params.setMargins(50,50,50,50);
+        achievmentsView.addView(iv,params);
 
     }
 
+    private void getFriendZone() {
+        if (friend) {
+            friendZone.setText("FRIEND");
+            friendZone.setTextColor(Color.GREEN);
+        } else {
+            friendZone.setText("NOT A FRIEND");
+            friendZone.setTextColor(Color.RED);
+        }
+    }
+
+    public void thumbsUpPressed(View v) {
+        int up = Integer.parseInt(thumbsUpText.getText().toString());
+        up += 1;
+        thumbsUpText.setText("" + up);
+    }
+
+    public void thumbsDownPressed(View v) {
+        int down = Integer.parseInt(thumbsDownText.getText().toString());
+        down += 1;
+        thumbsDownText.setText("" + down);
+    }
+
+    public void addFriendButton(View v) {
+        if(friend = false) {
+            friend = true;
+            getFriendZone();
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "TestUserPage Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.rickstestapp/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "TestUserPage Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.rickstestapp/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
---
+++ RicksExperiements/TestProject/app/src/main/java/group12/testproject/CreatePlayer.java
@@ -0,0 +1,108 @@
+package group12.testproject;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Icon;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class CreatePlayer extends AppCompatActivity {
+
+    private final static int SELECT_PHOTO = 12345;
+
+    public ImageButton userIconSelect;
+    public View currentView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_player);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        userIconSelect = (ImageButton) findViewById(R.id.userIconButton);
+    }
+
+
+
+    public void iconSelectPressed(View v){
+        currentView = v;
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent,SELECT_PHOTO);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Here we need to check if the activity that was triggers was the Image Gallery.
+        // If it is the requestCode will match the LOAD_IMAGE_RESULTS value.
+        // If the resultCode is RESULT_OK and there is some data we know that an image was picked.
+        if (requestCode == SELECT_PHOTO && resultCode == RESULT_OK && data != null) {
+            // Let's read picked image data - its URI
+            Uri pickedImage = data.getData();
+            // Let's read picked image path using content resolver
+            String[] filePath = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(pickedImage, filePath, null, null, null);
+            cursor.moveToFirst();
+            String imagePath = cursor.getString(cursor.getColumnIndex(filePath[0]));
+
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath, options);
+
+            BitmapDrawable d = new BitmapDrawable(getResources(), bitmap);
+
+            // Do something with th
+            //userIconSelect.setImageIcon(d);
+
+
+            // At the end remember to close the cursor or you will end with the RuntimeException!
+            cursor.close();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_create_player, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
