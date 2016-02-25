88d6b6d2a25db92836f93b9a6d9b1d2ae4b6c07c
Rick Twohy <twohyjr@iastate.edu>
2016-02-25 13:40:52 -0600

    Revert "Committing changes"
    This reverts commit 7ef0110a6d1a9eab3a128a5b502c6cd470080550.

--- MainProject/WhosPlayin/app/src/main/java/group12/whosplayin/LoginActivity.java
+++ MainProject/WhosPlayin/app/src/main/java/group12/whosplayin/LoginActivity.java
@@ -3,6 +3,7 @@ package group12.whosplayin;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -194,12 +195,13 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
-        return email.contains("@");
+        return true;
+        //return email.contains("@");
     }
 
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
-        return password.length() > 4;
+        return password.length() > 6;
     }
 
     /**
@@ -307,9 +309,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
 
-<<<<<<< HEAD
             boolean auth = false;
             User user = new User();
             try {
@@ -321,25 +321,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 Snackbar.make(mLoginFormView, e.getMessage(), Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
                 auth = false;
-=======
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
->>>>>>> parent of 5c8f4b8... Login activity now works and directs you to the main page once you are logged in
                 return false;
             }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            // TODO: register the new account here.
-            return true;
         }
 
         @Override
@@ -348,7 +331,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                finish();
+                //Create main screen activity
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
