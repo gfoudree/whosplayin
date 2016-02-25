25be240b9a832dde6a19c06658a0cfb72dbddb60
Grant Foudree <gfoudree@scylla001.student.iastate.edu>
2016-02-25 12:53:25 -0600

    Fixed bug and added navigation menu to profile fragment

--- MainProject/WhosPlayin/app/src/main/java/group12/whosplayin/ProfileFragment.java
+++ MainProject/WhosPlayin/app/src/main/java/group12/whosplayin/ProfileFragment.java
@@ -1,9 +1,9 @@
 package group12.whosplayin;
 
+import android.app.Fragment;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
