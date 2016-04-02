009f8e0c77e0e3700ba183c8fb2c96b655079539
Keegan Dwyer <kjdwyer@iastate.edu>
2016-04-02 13:36:03 -0500

    Added Report Player Page and started utility. Need to create table in database and add method to API

--- MainProject/WhosPlayin/app/src/main/java/group12/whosplayin/ReportActivity.java
+++ MainProject/WhosPlayin/app/src/main/java/group12/whosplayin/ReportActivity.java
@@ -2,13 +2,33 @@ package group12.whosplayin;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ReportActivity extends Activity {
+
+    private String reportName;
+    private String reportReason;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+
+        final EditText playerEditText = (EditText) findViewById(R.id.report_player);
+        final EditText reasonEditText = (EditText) findViewById(R.id.report_reason);
+        final Button reportSubmitButton =  (Button) findViewById(R.id.report_submit_button);
+
+        reportSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                contactConcern = concernEditText.getText().toString();
+                reportName = playerEditText.getText().toString();
+                reportReason = reasonEditText.getText().toString();
+
+            }
+        });
     }
 
 }
